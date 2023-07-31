9836489eea921ce69b58331b4110f6d9f3dd092e
Brecht_VDP <Brecht.vandepoele@ugent.be>
2023-07-31 20:26:10 +0200

    Cleaning upp

--- Robot Game/Game.js
+++ Robot Game/Game.js
@@ -7,6 +7,7 @@ let achtergrond;
 let distance = 0;
 let score = 0;
 let geluid1;
+let bugs = [];
 
 function preload() {
   achtergrond = loadImage('Media/cartoon-network-portal.png');
@@ -15,16 +16,26 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth - 10, windowHeight - 10);
+  achtergrond.resize(windowWidth-10, windowHeight-10);
   robot1 = new Robot();
   butter1 = new Butter();
-  achtergrond.resize(windowWidth-10, windowHeight-10);
+  for (let i = 0; i < 200; i++) {
+    bugs.push(new Jitter());
+  }
+  
 }
 
 function draw() {
   background(achtergrond);
+  for (let i = 0; i < bugs.length; i++) {
+    bugs[i].move();
+    bugs[i].display();
+  }
+
   push();
   rect(20, 20, 200, 50, 5);
   textSize(40);
+  
   text("Score: " + score, 35, 58);
   if(score == 10){
     if (geluid1.isPlaying()){
@@ -32,14 +43,17 @@ function draw() {
     }
     geluid1.play();
     score = 0;
-}
+  } 
   push();
   butter1.show();
   butter1.update();
   pop();
 
+  
+
   push();
   robot1.show();
   robot1.update();
   pop();
+  
 }
