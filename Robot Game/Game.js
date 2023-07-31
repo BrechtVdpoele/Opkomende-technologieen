0d702d096ac89b7f61bb8c9b41b7b4719a2a239e
Brecht_VDP <Brecht.vandepoele@ugent.be>
2023-07-31 12:38:28 +0200

    bubbles!

--- Robot Game/Game.js
+++ Robot Game/Game.js
@@ -37,6 +37,12 @@ function draw() {
     geluid1.play();
     score = 0
 }
+
+for (let i = 0; i < bugs.length; i++) {
+  bugs[i].move();
+  bugs[i].display();
+}
+
   push();
   strokeWeight(5);
   butter1.show();
@@ -47,22 +53,4 @@ function draw() {
   robot1.show();
   robot1.update();
   pop();
-
-  class Jitter {
-    constructor() {
-      this.x = random(width);
-      this.y = random(height);
-      this.diameter = random(10, 30);
-      this.speed = 1;
-    }
-  
-    move() {
-      this.x += random(-this.speed, this.speed);
-      this.y += random(-this.speed, this.speed);
-    }
-  
-    display() {
-      ellipse(this.x, this.y, this.diameter, this.diameter);
-    }
-  }
 }
